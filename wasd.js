74ded83ee4f55c0126e49cdf53288988c879240b
brbr <51085845+oujianzhang@users.noreply.github.com>
2021-10-19 12:04:58 +0200

    wasd complete

--- wasd.js
+++ wasd.js
@@ -1,75 +1,87 @@
+// socket used for speed
 var socketSpeed = io('http://127.0.0.1:5000/drive');
-let min = -127;
-let neutral = 0;
-let max = 127;
+var socketDirection = io('http://127.0.0.1:2001/drive');
 
-let speed = 0;
-let direction = 0;
-let speed_mod = 3.0;
-let dir_mod = 3.0;
+// minimum values
+var min_speed = 0.0;
+var min_direction = -1.0;
+// maximum values
+var max_speed = 1.0;
+var max_direction = 1.0;
+
+// speed slider
+var slider_speed = null;
+
+// key map
+var k_map = {};
 
 document.addEventListener('keydown', (e) => {
-    document.getElementById(e.key).classList.add('key-pressed');
+    //document.getElementById(e.key).classList.add('key-pressed');
     if (e.repeat) {
         // get pressed key
         var key = e.key;
         
-        // check if speed should be applied
-        if (key == 'w') {
-            speed += speed_mod;
-        } else if (key == 's') {
-            speed -= speed_mod;
+        // add key to map
+        k_map[key] = true;
+        
+        if(slider_speed == null){
+            slider_speed = document.getElementById("sliderSpeed");
+            // set slider max
+            slider_speed.max = max_speed * 100;
+            // set slider min
+            slider_speed.min = min_speed * 100;
+            // set slider starting position
+            slider_speed.value = (max_speed / 2) * 100;
         }
         
-        if (speed < min) {
-            speed = min;
-        } else if(speed > max) {
-            speed = max;
+        // speed value
+        let speed = 0.0;
+        // direction value
+        let dir = 0;
+        
+        // speed handling
+        if(k_map['w']){
+            speed = slider_speed.value / 100;
         }
-
-
-        if (key == 's') {
-            direction += dir_mod;
-        } else if (key == 'a') {
-            direction -= dir_mod;
+        else if(k_map['s']){
+            speed = slider_speed.value / 100 * -1;
         }
         
-        if (direction < min) {
-            direction = min;
-        } else if(direction > max) {
-            direction = max;
+        // direction handling
+        if(k_map['a']){
+            dir = -1;
+        }
+        else if(k_map['d']){
+            dir = 1;
         }
 
         // send speed value
         socketSpeed.emit('speed', { data: Math.round(speed) });
-        //socketDirection.emit('direction', { data: Math.round(direction) });
-        console.log("Speed    : " + speed);
-        console.log("Direction: " + direction);
-
+        socketDirection.emit('direciton', { data: dir });
+        
+        // debug output
+        console.log("Speed      : " + speed);
+        console.log("Direction  : " + dir);
     }
 });
 
 document.addEventListener('keyup', (e) => {
     if (!e.repeat) {
         var key = e.key;
-
-        if (key == 'w') {
-            speed = 0;
-            socketSpeed.emit('speed', { data: 0 });
-            document.getElementById(key).classList.remove('key-pressed');
-            console.log(key);
-        } else if (key == 's') {
-            speed = 0;
-            socketSpeed.emit('speed', { data: 0 });
-            document.getElementById(key).classList.remove('key-pressed');
-        } else if (key == 'a') {
-            direction = 0;
-            socketSpeed.emit('direction', { data: 0 });
-            document.getElementById(key).classList.remove('key-pressed');
-        } else if (key == 'd') {
-            direction = 0;
-            socketSpeed.emit('direction', { data: 0 });
-            document.getElementById(key).classList.remove('key-pressed');
+        
+        // remove key from map
+        k_map[key] = false;
+        
+        switch(key){
+            case 'w':
+            case 's':
+                socketSpeed.emit('speed', { data: 0 });
+                break;
+                
+            case 'a':
+            case 'd':
+                socketDirection.emit('direciton', { data: 0 });
+                break;
         }
     }
 });
